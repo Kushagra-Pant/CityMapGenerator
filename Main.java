90cefd3b0e01cb19fcf430f7018445f80bcf6eb6
Kushagra Pant <154854749+Kushagra-Pant@users.noreply.github.com>
2024-06-21 22:00:10 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
   public static void main(String[] args) {
+    // This is just an example
     City Maxtopia = new City("Maxtopia");
     System.out.println(Maxtopia.getName());
   }
