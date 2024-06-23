0996c6e029a25e56244368c3d1d7cea5c2508377
Kushagra Pant <154854749+Kushagra-Pant@users.noreply.github.com>
2024-06-22 20:14:36 -0400

    Update City.java

--- City.java
+++ City.java
@@ -1,4 +1,8 @@
-public class City{
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Graphics;
+
+public class City extends JPanel {
   private String name;
   private Road roads[] = new Road[0];
   
@@ -17,17 +21,37 @@ public class City{
   public void setName(String newName){
     this.name = newName;
   }
+  
+  public void draw(){
+    JFrame frame = new JFrame("Simple Graphics Example");
+    CityPanel panel = new CityPanel();
+
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(400, 400);
+    frame.add(panel);
+    frame.setVisible(true);
+  }
+  
+  class CityPanel extends JPanel {
+      @Override
+      protected void paintComponent(Graphics g) {
+          super.paintComponent(g);
+          for (int i = 0; i < roads.length; i++) {
+              roads[i].draw(g, getWidth(), getHeight());
+          }
+      }
+  }
 
   public void createRoad(int startingEastCoordinate, int startingNorthCoordinate, int endingEastCoordinate, int endingNorthCoordinate){
     Road[] newRoads = new Road[roads.length + 1];
     for(int i = 0; i < roads.length; i++){
       newRoads[i] = roads[i];
     }
-    newRoads[newRoads.length - 1] = new Road(startingEastCoordinate, startingNorthCoordinate, endingEastCoordinate, endingNorthCoordinate);
+    newRoads[newRoads.length - 1] = new Road(startingEastCoordinate, startingNorthCoordinate, endingEastCoordinate, endingNorthCoordinate, this);
     roads = newRoads;
   }
 
-  private int[] getMin(){
+  public int[] getMin(){
     int[] min = new int[2];
     min[0] = roads[0].getMin()[0];
     min[1] = roads[0].getMin()[1];
@@ -42,7 +66,7 @@ public class City{
     return min;
   }
 
-  private int[] getMax(){
+  public int[] getMax(){
     int[] max = new int[2];
     max[0] = roads[0].getMax()[0];
     max[1] = roads[0].getMax()[0];
