54f47bbe756bf5f437d6abd145e639005701b117
Kushagra Pant <154854749+Kushagra-Pant@users.noreply.github.com>
2024-06-22 20:14:50 -0400

    Update Road.java

--- Road.java
+++ Road.java
@@ -1,12 +1,57 @@
+import java.awt.Graphics;
+import java.awt.Color;
+
 public class Road {
   private int[] xVals = new int[2];
   private int[] yVals = new int[2];
+  private City city;
 
-  public Road(int startingEastCoordinate, int startingNorthCoordinate, int endingEastCoordinate, int endingNorthCoordinate){
+  public Road(int startingEastCoordinate, int startingNorthCoordinate, int endingEastCoordinate, int endingNorthCoordinate, City parentCity){
     xVals[0] = startingEastCoordinate;
     xVals[1] = endingEastCoordinate;
     yVals[0] = startingNorthCoordinate;
     yVals[1] = endingNorthCoordinate;
+    city = parentCity;
+  }
+  
+  public void draw(Graphics g, int frameWidth, int frameHeight) {
+    double width = city.getMax()[0] - city.getMin()[0];
+    double height = city.getMax()[1] - city.getMin()[1];
+    double scale = Math.min(frameWidth/width, frameHeight/height);
+    g.setColor(Color.BLACK);
+
+    for(int i = 1; i < xVals.length; i++){
+      g.drawLine((int)((xVals[i - 1] - city.getMin()[0])*scale + 0.5),
+                 (int)((yVals[i - 1] - city.getMin()[0])*scale + 0.5),
+                 (int)((xVals[i] - city.getMin()[0])*scale + 0.5),
+                 (int)((yVals[i] - city.getMin()[0])*scale + 0.5));
+    }
+  }
+
+  public void extendTo(int newEndEastCoordinate, int newEndNorthCoordinate){
+    int[] newXVals = new int[xVals.length + 1];
+    int[] newYVals = new int[yVals.length + 1];
+    for(int i = 0; i < xVals.length; i++){
+      newXVals[i] = xVals[i];
+      newYVals[i] = yVals[i];
+    }
+    newXVals[newXVals.length - 1] = newEndEastCoordinate;
+    newYVals[newYVals.length - 1] = newEndNorthCoordinate;
+    xVals = newXVals;
+    yVals = newYVals;
+  }
+
+  public void extendBehind(int newStartEastCoordinate, int newStartNorthCoordinate){
+    int[] newXVals = new int[xVals.length + 1];
+    int[] newYVals = new int[yVals.length + 1];
+    for(int i = 1; i < xVals.length; i++){
+      newXVals[i] = xVals[i];
+      newYVals[i] = yVals[i];
+    }
+    newXVals[0] = newStartEastCoordinate;
+    newYVals[0] = newStartNorthCoordinate;
+    xVals = newXVals;
+    yVals = newYVals;
   }
   
   /**
