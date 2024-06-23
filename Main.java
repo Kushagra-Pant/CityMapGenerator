a25ce703615880c22c53e516ba5accdc6691eb96
Kushagra Pant <154854749+Kushagra-Pant@users.noreply.github.com>
2024-06-22 20:15:16 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,10 @@ public class Main {
   public static void main(String[] args) {
     // This is just an example
     City Maxtopia = new City("Maxtopia");
-    System.out.println(Maxtopia.getName());
+    Maxtopia.createRoad(50, 50, 200, 200);
+    Maxtopia.createRoad(150, 100, 900, 900);
+    System.out.println(Maxtopia.getMin()[0] + " " + Maxtopia.getMin()[1]);
+    System.out.println(Maxtopia.getMax()[0] + " " + Maxtopia.getMax()[1]);
+    Maxtopia.draw();
   }
 }
